8b9899360ffed2be7ee5626198377981dad336d1
flysquare <umutkonurinso@gmail.com>
2023-02-15 15:16:54 +0300

    added layouts service model and components logic

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {PostComponent} from "./pages/post/post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'post/:slug', component: PostComponent },
+  { path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
